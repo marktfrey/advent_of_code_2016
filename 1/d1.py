c75bc6049446236192b630efc105ace90b3bf6ae
Mark Frey <mfrey@thinkthroughmath.com>
2016-12-17 08:12:26 -0500

    AoC d1 refactor

--- 1/d1.py
+++ 1/d1.py
@@ -1,4 +1,3 @@
-# State
 DIR_MAP = {
     'N':
       { 'R': 'E',
@@ -14,35 +13,8 @@ DIR_MAP = {
         'L': 'S' }
   }
 
-def new_facing(facing, turn):
-  return DIR_MAP[facing][turn]
-
-def new_position(facing, distance, pos):
-  if facing == 'N':
-    return [pos[0] + distance, pos[1]]
-  elif facing == 'E':
-    return [pos[0], pos[1] + distance]
-  elif facing == 'S':
-    return [pos[0] - distance, pos[1]]
-  elif facing == 'W':
-    return [pos[0], pos[1] - distance]
-
 def distance_from_origin(pos):
-  return abs(pos[0]) + abs(pos[1])
-
-def ending_position(path):
-  facing   = 'N'
-  position = [0, 0]
-
-  for d in path.split(","):
-    d = d.strip()
-    turn     = d[0]
-    distance = int(d[1: ])
-
-    facing = new_facing(facing, turn)
-    position = new_position(facing, distance, position)
-
-  return position
+  return None if pos is None else abs(pos[0]) + abs(pos[1])
 
 def visited_positions(facing, distance, pos):
   visited = []
@@ -59,36 +31,28 @@ def visited_positions(facing, distance, pos):
 
   return visited
 
-def first_revisited_position(path):
-  facing   = 'N'
-  current_pos = [0, 0]
-  past_pos    = [[0, 0]]
+def traversal(path):
+  facing      = 'N'
+  prior_pos   = [[0, 0]]
 
   for d in path.split(","):
     d = d.strip()
-    turn     = d[0]
-    distance = int(d[1: ])
+    facing    =  DIR_MAP[facing][d[0]]
+    prior_pos += visited_positions(facing, int(d[1: ]), prior_pos[-1])
 
-    facing        = new_facing(facing, turn)
-    new_positions = visited_positions(facing, distance, current_pos)
+  return prior_pos
 
-    for new_pos in new_positions:
-      if new_pos in past_pos:
-        return new_pos
-      else:
-        past_pos.append(new_pos)
-
-    current_pos = past_pos[-1]
+def first_revisited_position(path):
+  visited = traversal(path)
+  for i, pos in enumerate(visited):
+    if pos in visited[0:i]:
+      return pos
   return None
 
-def first_revisited_distance_from_origin(path):
-  first_pos = first_revisited_position(path)
-
-  if first_pos is None:
-    return None
-  else:
-    return distance_from_origin(first_pos)
-
 def ending_distance_from_origin(path):
-  pos = ending_position(path)
+  pos = traversal(path)[-1]
+  return distance_from_origin(pos)
+
+def first_revisited_distance_from_origin(path):
+  pos = first_revisited_position(path)
   return distance_from_origin(pos)
--- 1/d1_input.py
+++
@@ -1,6 +0,0 @@
-import d1
- 
-path = input('Enter the problem input: ')
-final = d1.ending_distance_from_origin(path)
-revis = d1.first_revisited_distance_from_origin(path)
-print('ANSWER: \n my final distance is %(final)d from origin.\n My first revisited position was %(revis)s' % locals())
